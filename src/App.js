7cf5e439e103315bb400077ef9db64c465f50bbb
Vainiusst <vainius1994@gmail.com>
2020-09-21 21:46:50 +0300

    update to the project - UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageInput from './Components/ImageInput/ImageInput';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const particlesOpts = {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      input: ""
+    }
+  };
+
+  onInputChange = (event) => console.log(event.target.value);
+  
+  onButtonSubmit = () => console.log("click");
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOpts}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageInput
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}/>
+              {/*
+              <FaceRecignition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
